ddf83f9d3519c2f1c9b3699bed30a80cf9aa819a
Unknown <mehrdad.shobeyri@yahoo.com>
2017-05-22 15:34:41 +0430

    remvoe namespaces

--- src/media_management/videoencoderconfiguration.cpp
+++ src/media_management/videoencoderconfiguration.cpp
@@ -9,40 +9,40 @@ VideoEncoderConfiguration::~VideoEncoderConfiguration() {}
 QDomElement
 VideoEncoderConfiguration::toxml() {
     QDomElement setVideoConfiguration, configuration, width, height, name,
-        token, useCount, encoding, resolution, quality, rateControl,
+        useCount, encoding, resolution, quality, rateControl,
         frameRateLimit, encodingInterval, bitrateLimit, h264, h264profile,
         govLength, multicast, address, type, ipv4Address, port, ttl, autoStart,
         sessionTimeout, forcePresistence;
-    setVideoConfiguration = newElement("wsdl:SetVideoEncoderConfiguration");
-    configuration         = newElement("trt:Configuration");
-    name                  = newElement("tt:Name", this->name());
-    useCount    = newElement("tt:UseCount", QString::number(this->useCount()));
-    encoding    = newElement("tt:Encoding", this->encoding());
-    resolution  = newElement("tt:Resolution");
-    width       = newElement("tt:Width", QString::number(this->width()));
-    height      = newElement("tt:Height", QString::number(this->height()));
-    quality     = newElement("tt:Quality", QString::number(this->quality()));
-    rateControl = newElement("tt:RateControl");
-    frameRateLimit = newElement(
-        "tt:FrameRateLimit", QString::number(this->frameRateLimit()));
+
     setVideoConfiguration = newElement("SetVideoEncoderConfiguration");
+    configuration = newElement("Configuration");
+    name          = newElement("Name", this->name());
+    useCount      = newElement("UseCount", QString::number(this->useCount()));
+    encoding      = newElement("Encoding", this->encoding());
+    resolution    = newElement("Resolution");
+    width         = newElement("Width", QString::number(this->width()));
+    height        = newElement("Height", QString::number(this->height()));
+    quality       = newElement("Quality", QString::number(this->quality()));
+    rateControl   = newElement("RateControl");
+    frameRateLimit =
+        newElement("FrameRateLimit", QString::number(this->frameRateLimit()));
     encodingInterval = newElement(
-        "tt:EncodingInterval", QString::number(this->encodingInterval()));
+        "EncodingInterval", QString::number(this->encodingInterval()));
     bitrateLimit =
-        newElement("tt:BitrateLimit", QString::number(this->bitrateLimit()));
-    h264      = newElement("tt:H264");
-    govLength = newElement("tt:GovLength", QString::number(this->govLength()));
-    h264profile = newElement("tt:H264Profile", this->h264Profile());
-    multicast   = newElement("tt:Multicast");
-    address     = newElement("tt:Address");
-    type        = newElement("tt:Type", this->type());
-    ipv4Address = newElement("tt:IPv4Address", this->ipv4Address());
-    port        = newElement("tt:Port", QString::number(this->port()));
-    ttl         = newElement("tt:TTL", QString::number(this->ttl()));
-    autoStart   = newElement(
-        "tt:AutoStart", this->autoStart() == true ? "true" : "false");
-    sessionTimeout   = newElement("tt:SessionTimeout", this->sessionTimeout());
-    forcePresistence = newElement("tt:ForcePersistence", "true");
+        newElement("BitrateLimit", QString::number(this->bitrateLimit()));
+    h264        = newElement("H264");
+    govLength   = newElement("GovLength", QString::number(this->govLength()));
+    h264profile = newElement("H264Profile", this->h264Profile());
+    multicast   = newElement("Multicast");
+    address     = newElement("Address");
+    type        = newElement("Type", this->type());
+    ipv4Address = newElement("IPv4Address", this->ipv4Address());
+    port        = newElement("Port", QString::number(this->port()));
+    ttl         = newElement("TTL", QString::number(this->ttl()));
+    autoStart =
+        newElement("AutoStart", this->autoStart() == true ? "true" : "false");
+    sessionTimeout   = newElement("SessionTimeout", this->sessionTimeout());
+    forcePresistence = newElement("ForcePersistence", "true");
 
     setVideoConfiguration.setAttribute(
         "xmlns", "http://www.onvif.org/ver10/media/wsdl");
@@ -65,7 +65,7 @@ VideoEncoderConfiguration::toxml() {
     h264.appendChild(govLength);
     h264.appendChild(h264profile);
     address.appendChild(type);
-//    address.appendChild(ipv4Address);
+    //    address.appendChild(ipv4Address);
     multicast.appendChild(address);
     multicast.appendChild(port);
     multicast.appendChild(ttl);
@@ -83,5 +83,6 @@ VideoEncoderConfiguration::toxml() {
 
     setVideoConfiguration.appendChild(configuration);
     setVideoConfiguration.appendChild(forcePresistence);
+
     return setVideoConfiguration;
 }
